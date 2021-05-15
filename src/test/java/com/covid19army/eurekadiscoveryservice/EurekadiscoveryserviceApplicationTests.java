4594695db005dd87e7b58aa345aaff3f477adcad
Ravi Kumar K G <ravi.rk2406@gmail.com>
2021-05-15 10:14:23 +0000

    eureka discovery initial code commit

---
+++ src/test/java/com/covid19army/eurekadiscoveryservice/EurekadiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid19army.eurekadiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekadiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
